6f26f930bb3936b683d87aa918652c39885456b1
ZairBalam <fzairb2@gmail.com>
2025-05-12 22:12:55 -0600

    guide and camp

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -22,8 +22,8 @@ const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps)
           />
         </div>
         <div className="flex flex-col gap-1">
-          <h4 className="bold-18 text-white">{title}</h4>
-          <p className="regular-14 text-white">{subtitle}</p>
+          <h4 className="bold-18 text-white" style={{ textShadow: '1px 1px 2px black' }}>{title}</h4>
+          <p className="regular-14 text-white"style={{ textShadow: '1px 1px 2px black' }}>{subtitle}</p>
         </div>
       </div>
 
@@ -40,7 +40,7 @@ const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps)
             />
           ))}
         </span>
-        <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
+        <p className="bold-16 md:bold-20 text-white"style={{ textShadow: '1px 1px 2px black' }}>{peopleJoined}</p>
       </div>
      </div>
     </div>
@@ -53,25 +53,25 @@ const Camp = () => {
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite 
           backgroundImage="bg-bg-img-1"
-          title="Putuk Truno Camp"
-          subtitle="Prigen, Pasuruan"
-          peopleJoined="50+ Joined"
+          title="Hunucmá Camp Area"
+          subtitle="Yucatan, Mexico"
+          peopleJoined="143+ Joined"
         />
         <CampSite 
           backgroundImage="bg-bg-img-2"
-          title="Mountain View Camp"
-          subtitle="Somewhere in the Wilderness"
-          peopleJoined="50+ Joined"
+          title="Sisal Beach Camp"
+          subtitle="Yucatan, Mexico"
+          peopleJoined="87+ Joined"
         />
       </div>
 
       <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
         <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
-          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
-            <strong>Feeling Lost</strong> And Not Knowing The Way?
+          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white"style={{ textShadow: '1px 1px 2px black' }}>
+            <strong>Feeling lost and unsure where to go?</strong> 
           </h2>
-          <p className="regular-14 xl:regular-16 mt-5 text-white">
-            Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure
+          <p className="regular-16 xl:regular-16 mt-5 text-white">
+            The anxiety of visiting a new place to swim can be overwhelming — the chances of getting lost are high. That’s why we’re here: to guide adventurers like you from the very first step.
           </p>
           <Image 
             src="/quote.svg"
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,9 +14,9 @@ const Hero = () => {
           height={50}
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
-        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
+        <h1 className="bold-52 lg:bold-88"> Hunucmá Camp Area</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-          We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
+          We want to be with you on every step of your journey, seeking the incorruptible beauty of nature. Explore the whole world with just one app — your ultimate adventure companion.
         </p>
 
         <div className="my-11 flex flex-wrap gap-5">
@@ -33,7 +33,7 @@ const Hero = () => {
           </div>
 
           <p className="bold-16 lg:bold-20 text-blue-70">
-            198k
+            296k
             <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
           </p>
         </div>
@@ -61,16 +61,16 @@ const Hero = () => {
               <p className="regular-16 text-gray-20">Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
-            <p className="bold-20 text-white">Aguas Calientes</p>
+            <p className="bold-20 text-white">Hunucmá Yucatán</p>
           </div>
 
           <div className="flexBetween">
             <div className="flex flex-col">
               <p className="regular-16 block text-gray-20">Distance</p>
-              <p className="bold-20 text-white">173.28 mi</p>
+              <p className="bold-20 text-white">24.58 km</p>
             </div>
             <div className="flex flex-col">
-              <p className="regular-16 block text-gray-20">Elevation</p>
+              <p className="regular-16 block text-gray-20">Cenote</p>
               <p className="bold-20 text-white">2.040 km</p>
             </div>
           </div>
